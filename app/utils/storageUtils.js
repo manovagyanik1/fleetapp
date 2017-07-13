6190930222e3375c2622bca784b8c9a64c871861
shubham agrawal <manovagyanik1@gmail.com>
2017-07-14 01:52:09 +0530

    adding storageUtils

---
+++ app/utils/storageUtils.js
@@ -0,0 +1,21 @@
+import Storage from 'react-native-storage';
+import { AsyncStorage } from 'react-native';
+
+const storage = new Storage({
+    size: 1000,
+    storageBackend: AsyncStorage,
+
+    // expire time, default 1 day(1000 * 3600 * 24 milliseconds).
+    // can be null, which means never expire.
+    defaultExpires: 1000 * 3600 * 24,
+
+    // cache data in the memory. default is true.
+    enableCache: true,
+
+    // if data was not found in storage or expired,
+    // the corresponding sync method will be invoked and return
+    // the latest data.
+    sync : {
+        // we'll talk about the details later.
+    }
+})
