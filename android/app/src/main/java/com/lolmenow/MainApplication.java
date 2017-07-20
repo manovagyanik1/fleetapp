6bf9c3b13d65ee92c1325197e4d64e8d31942ab2
shubham agrawal <manovagyanik1@gmail.com>
2017-07-20 19:36:27 +0530

    adding crash analytics

--- android/app/src/main/java/com/lolmenow/MainApplication.java
+++ android/app/src/main/java/com/lolmenow/MainApplication.java
@@ -6,6 +6,7 @@ import com.facebook.CallbackManager;
 import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
 import com.facebook.react.ReactApplication;
+import io.sentry.RNSentryPackage;
 import com.geektime.rnonesignalandroid.ReactNativeOneSignalPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.idehub.GoogleAnalyticsBridge.GoogleAnalyticsBridgePackage;
@@ -46,6 +47,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
               new MainReactPackage(),
+            new RNSentryPackage(MainApplication.this),
             new ReactNativeOneSignalPackage(),
             new LinearGradientPackage(),
             new GoogleAnalyticsBridgePackage(),
--- index.android.js
+++ index.android.js
@@ -1,4 +1,9 @@
 import { AppRegistry } from 'react-native';
 import App from './app/index';
 
+import { Sentry } from 'react-native-sentry';
+
+Sentry.config("https://0d97517af38b4beeb0d6a50398f9d1a4:9d6553eb33d94564b25a96c4daf8afa0@sentry.io/193878").install();
+
+
 AppRegistry.registerComponent('LolMeNow', () => App);
